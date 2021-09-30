0c36c7593935fa3baf5858da45fe9633812aa52c
Martín Triay <martriay@gmail.com>
2021-09-30 20:57:58 -0300

    make imports dynamic. fix indentation

--- src/nile/commands/compile.py
+++ src/nile/commands/compile.py
@@ -37,10 +37,10 @@ def _compile_contract(path):
     print(f"🔨 Compiling {path}")
 
     cmd = f"""
-  starknet-compile {path} \
-    --cairo_path={CONTRACTS_DIRECTORY}
-    --output {BUILD_DIRECTORY}{filename}.json \
-    --abi {ABIS_DIRECTORY}{filename}.json
-  """
+    starknet-compile {path} \
+        --cairo_path={CONTRACTS_DIRECTORY}
+        --output {BUILD_DIRECTORY}{filename}.json \
+        --abi {ABIS_DIRECTORY}{filename}.json
+    """
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
