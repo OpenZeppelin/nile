df56ce017cff3960824bb416fb15c7ae621db58d
Martín Triay <martriay@gmail.com>
2021-11-04 03:44:04 -0300

    add starknet-devnet to install

--- src/nile/commands/install.py
+++ src/nile/commands/install.py
@@ -6,5 +6,5 @@ import sys
 def install_command():
     """Install Cairo package with the given tag."""
     print("🗄  Installing Cairo")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "cairo-lang"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "cairo-lang", "starknet-devnet"])
     print("✨  Cairo successfully installed!")
