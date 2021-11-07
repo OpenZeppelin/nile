273a71810364366dfdbee52249c1c5c220c04e78
Martín Triay <martriay@gmail.com>
2021-11-07 19:05:05 -0300

    handle missing starknet-devnet

--- src/nile/commands/node.py
+++ src/nile/commands/node.py
@@ -4,4 +4,10 @@ import subprocess
 
 def node_command():
     """Start StarkNet local network."""
-    subprocess.check_call("starknet-devnet")
+    try:
+        subprocess.check_call("starknet-devnet")
+    except FileNotFoundError:
+        print("")
+        print("😰 Could not find starknet-devnet, are you sure it is installed? Try with:\n")
+        print("   pip install starknet-devnet")
+        print("")
