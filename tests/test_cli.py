09141918d6022e8dcb11b51f21e9b11fb8352695
Andrew <fleming-andrew@protonmail.com>
2022-04-01 01:19:06 -0400

    fix gateway_url in cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -167,6 +167,6 @@ def test_locate_error(mock_subprocess, args):
     # Setup and assert expected output
     expected = ["starknet", "tx_status", "--hash", MOCK_HASH]
     if len(args) == 1:
-        expected.append("--feeder_gateway_url=None")
+        expected.append("--feeder_gateway_url=http://127.0.0.1:5000/")
 
     mock_subprocess.check_output.assert_called_once_with(expected)
