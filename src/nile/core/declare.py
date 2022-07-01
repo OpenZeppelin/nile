b5b1938d10d777168bf275c88d6b454a5228fbfa
Andrew <fleming-andrew@protonmail.com>
2022-07-01 19:44:34 -0400

    check alias before tx

--- src/nile/core/declare.py
+++ src/nile/core/declare.py
@@ -2,12 +2,16 @@
 import logging
 
 from nile import deployments
-from nile.common import parse_information, run_command
+from nile.common import DECLARATIONS_FILENAME, parse_information, run_command
 
 
 def declare(contract_name, network, alias=None, overriding_path=None):
     """Declare StarkNet smart contracts."""
     logging.info(f"🚀 Declaring {contract_name}")
+
+    if alias_exists(alias, network):
+        file = f"{network}.{DECLARATIONS_FILENAME}"
+        raise Exception(f"Alias {alias} already exists in {file}")
 
     output = run_command(contract_name, network, overriding_path, operation="declare")
     class_hash, tx_hash = parse_information(output)
@@ -16,3 +20,9 @@ def declare(contract_name, network, alias=None, overriding_path=None):
 
     deployments.register_class_hash(class_hash, network, alias)
     return class_hash
+
+
+def alias_exists(alias, network):
+    """Return whether an alias exists or not."""
+    existing_alias = next(deployments.load_class(alias, network), None)
+    return existing_alias is not None
