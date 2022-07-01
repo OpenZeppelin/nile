e1f62430a5a6e5e5fe18c4bb6c1a83ce85703c56
Andrew <fleming-andrew@protonmail.com>
2022-06-30 23:32:55 -0400

    add conditional for host

--- src/nile/core/node.py
+++ src/nile/core/node.py
@@ -10,7 +10,10 @@ def node(host="127.0.0.1", port=5050):
     try:
         # Save host and port information to be used by other commands
         file = NODE_FILENAME
-        network = host
+        if host == "127.0.0.1":
+            network = "localhost"
+        else:
+            network = host
         gateway_url = f"http://{host}:{port}/"
         gateway = {network: gateway_url}
 
