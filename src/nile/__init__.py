613f804a339e92ed30933bc391418f4f73b9e77b
JulissaDantes <julissadcj@gmail.com>
2022-07-01 17:37:53 -0400

    Update __init__.py

--- src/nile/__init__.py
+++ src/nile/__init__.py
@@ -6,6 +6,6 @@ except ImportError:
     import importlib_metadata
 
 try:
-    __version__ = importlib_metadata.version("cairo-nile")
+    __version__ = importlib_metadata.version("juli-nile")
 except importlib_metadata.PackageNotFoundError:
     __version__ = None
