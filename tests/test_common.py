9b71ba0f95cf3025e36ffb6e8f19c1d2876013de
Andrew <fleming-andrew@protonmail.com>
2022-07-27 00:39:07 -0400

    add no_wallet to command

---
+++ tests/test_common.py
@@ -0,0 +1,45 @@
+"""Tests for deploy command."""
+import logging
+from unittest.mock import patch
+import os
+import pytest
+
+from nile.common import run_command, BUILD_DIRECTORY
+
+
+CONTRACT = "contract"
+OPERATION = "invoke"
+NETWORK = "goerli"
+ARGS = [1, 2, 3]
+
+@pytest.fixture(autouse=True)
+def tmp_working_dir(monkeypatch, tmp_path):
+    monkeypatch.chdir(tmp_path)
+    return tmp_path
+
+
+@pytest.mark.parametrize(
+    "operation", ["invoke", "call"]
+)
+@patch("nile.common.subprocess.check_output")
+def test_run_command(mock_subprocess, operation):
+    logging.getLogger().setLevel(logging.INFO)
+
+    run_command(
+        contract_name=CONTRACT,
+        network=NETWORK,
+        operation=operation,
+        arguments=ARGS
+    )
+
+    mock_subprocess.assert_called_once_with([
+        "starknet",
+        operation,
+        "--contract",
+        f"{BUILD_DIRECTORY}/{CONTRACT}.json",
+        "--inputs",
+        *ARGS,
+        "--no_wallet"
+    ])
+
+
