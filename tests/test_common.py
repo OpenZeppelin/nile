58b2dccdd64a36ef9fbdbbac26bc6a0c1b63bd79
Andrew <fleming-andrew@protonmail.com>
2022-07-27 00:53:01 -0400

    fix formatting

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,45 +1,40 @@
 """Tests for deploy command."""
 import logging
 from unittest.mock import patch
-import os
+
 import pytest
 
-from nile.common import run_command, BUILD_DIRECTORY
-
+from nile.common import BUILD_DIRECTORY, run_command
 
 CONTRACT = "contract"
 OPERATION = "invoke"
 NETWORK = "goerli"
 ARGS = [1, 2, 3]
 
-@pytest.fixture(autouse=True)
-def tmp_working_dir(monkeypatch, tmp_path):
-    monkeypatch.chdir(tmp_path)
-    return tmp_path
+
+#@pytest.fixture(autouse=True)
+#def tmp_working_dir(monkeypatch, tmp_path):
+#    monkeypatch.chdir(tmp_path)
+#    return tmp_path
 
 
-@pytest.mark.parametrize(
-    "operation", ["invoke", "call"]
-)
+@pytest.mark.parametrize("operation", ["invoke", "call"])
 @patch("nile.common.subprocess.check_output")
 def test_run_command(mock_subprocess, operation):
     logging.getLogger().setLevel(logging.INFO)
 
     run_command(
-        contract_name=CONTRACT,
-        network=NETWORK,
-        operation=operation,
-        arguments=ARGS
+        contract_name=CONTRACT, network=NETWORK, operation=operation, arguments=ARGS
     )
 
-    mock_subprocess.assert_called_once_with([
-        "starknet",
-        operation,
-        "--contract",
-        f"{BUILD_DIRECTORY}/{CONTRACT}.json",
-        "--inputs",
-        *ARGS,
-        "--no_wallet"
-    ])
-
-
+    mock_subprocess.assert_called_once_with(
+        [
+            "starknet",
+            operation,
+            "--contract",
+            f"{BUILD_DIRECTORY}/{CONTRACT}.json",
+            "--inputs",
+            *ARGS,
+            "--no_wallet",
+        ]
+    )
