13f0e99a8a8b2d9ee993a260db63441de41ee1c8
Martín Triay <martriay@gmail.com>
2022-08-04 23:29:51 -0300

    improve logging

--- src/nile/core/call_or_invoke.py
+++ src/nile/core/call_or_invoke.py
@@ -51,14 +51,19 @@ def call_or_invoke(
     except subprocess.CalledProcessError:
         p = subprocess.Popen(command, stderr=subprocess.PIPE)
         _, error = p.communicate()
+        err_msg = error.decode()
 
-        if "max_fee must be bigger than 0" in error.decode():
+        if "max_fee must be bigger than 0" in err_msg:
             logging.error(
                 """
                 \n😰 Whoops, looks like max fee is missing. Try with:\n
                 --max_fee=`MAX_FEE`
                 """
             )
-            return ""
-        else:
-            raise
+        elif "transactions should go through the __execute__ entrypoint." in err_msg:
+            logging.error(
+                "\n\n😰 Whoops, looks like you're not using an account. Try with:\n"
+                "\nnile send [OPTIONS] SIGNER CONTRACT_NAME METHOD [PARAMS]"
+            )
+
+        return ""
