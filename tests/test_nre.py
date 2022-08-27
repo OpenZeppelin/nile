99f4697a21058c89b28d0286f3dc744ebf4d5f65
Andrew <fleming-andrew@protonmail.com>
2022-08-26 22:22:34 -0400

    test nre get-accounts msgs

--- tests/test_nre.py
+++ tests/test_nre.py
@@ -3,7 +3,6 @@ Tests for nre module.
 
 Only unit tests for now.
 """
-
 from unittest.mock import patch
 
 import click
@@ -31,3 +30,29 @@ def test_nre_loaded_plugins():
 
         nre_result = nre.dummy_params(["1", "2"])
         assert 3 == nre_result
+
+
+def test_nre_get_accounts(capsys):
+    nre = NileRuntimeEnvironment()
+    with patch("nile.nre.get_accounts", return_value=(123)):
+        res = nre.get_accounts()
+        captured = capsys.readouterr()
+
+        assert res == (123)
+        assert str(captured.out) == ""
+        assert str(captured.err) == ""
+
+
+def test_nre_get_accounts_without_registered_accounts(capsys):
+    nre = NileRuntimeEnvironment()
+
+    res = nre.get_accounts()
+    captured = capsys.readouterr()
+
+    assert res is None
+    assert str(captured.err) == ""
+    assert "❌ No registered accounts detected in localhost.accounts.json" in str(
+        captured.out
+    )
+    assert "Try 'nile setup <private key alias>'" in str(captured.out)
+    assert "For more info, see Nile's 'get-accounts' in README" in str(captured.out)
