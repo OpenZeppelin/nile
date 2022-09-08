c4996a4ccb47cddac85a232522b613a965cb2cb1
Martín Triay <martriay@gmail.com>
2022-09-08 20:53:01 -0300

    add missing docstring

--- src/nile/signer.py
+++ src/nile/signer.py
@@ -80,6 +80,7 @@ def get_transaction_hash(prefix, account, calldata, nonce, max_fee):
 
 
 def get_raw_invoke(sender, calls):
+    """Construct and return StarkNet's internal raw_invocation."""
     call_array, calldata = from_call_to_call_array(calls)
     raw_invocation = sender.__execute__(call_array, calldata)
     return raw_invocation
