b59a4f7df6489c32418e547b287d9c07ebd6686c
Eric Nordelo <eric.nordelo39@gmail.com>
2022-09-20 14:44:30 +0200

    fix: run format and linter

--- tests/commands/test_get_nonce.py
+++ tests/commands/test_get_nonce.py
@@ -1,10 +1,9 @@
 """Tests for get-nonce command."""
-
+import logging
+import os
 from unittest.mock import patch
 
-import logging
 import pytest
-import os
 
 from nile.utils.get_nonce import get_nonce
 
@@ -22,21 +21,21 @@ def test_call_format(mock_gateway, mock_subprocess, contract_address, network):
 
     command = ["starknet", "get_nonce", "--contract_address", contract_address]
     if network == "localhost":
-        command.append(f"--feeder_gateway_url=http://127.0.0.1:5050/")
+        command.append("--feeder_gateway_url=http://127.0.0.1:5050/")
     else:
-      assert os.getenv("STARKNET_NETWORK") == f"alpha-{network}"
+        assert os.getenv("STARKNET_NETWORK") == f"alpha-{network}"
 
     mock_subprocess.assert_called_once_with(command)
 
 
-@patch("nile.core.node.subprocess.check_output", return_value='5')
+@patch("nile.core.node.subprocess.check_output", return_value="5")
 @patch("nile.common.get_gateway", return_value=GATEWAYS)
 def test_deploy(mock_gateway, mock_subprocess, caplog):
     logging.getLogger().setLevel(logging.INFO)
 
     # check return values
-    nonce = get_nonce('0xffff', 'localhost')
+    nonce = get_nonce("0xffff", "localhost")
     assert nonce == 5
 
     # check logs
-    assert f"Current nonce for 0xffff is 5" in caplog.text
+    assert "Current nonce for 0xffff is 5" in caplog.text
