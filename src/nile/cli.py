2a2710fea156ca3bc1b822546f5581708acc6fd2
Eric Nordelo <eric.nordelo39@gmail.com>
2022-10-03 19:24:01 +0200

    Update Account.send API (#218)
    * fix: update Account.send API
    
    * fix: typo

--- src/nile/cli.py
+++ src/nile/cli.py
@@ -126,8 +126,7 @@ def send(signer, address_or_alias, method, params, network, max_fee=None):
             method, address_or_alias, [x for x in params]
         )
     )
-    if not is_alias(address_or_alias):
-        address_or_alias = normalize_number(address_or_alias)
+    # Account.send is part of the public API, so it accepts addresses as string
     out = account.send(address_or_alias, method, params, max_fee=max_fee)
     print(out)
 
