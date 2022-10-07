981255d1d29efa58297b281014526c58b9c88d8d
Eric Nordelo <eric.nordelo39@gmail.com>
2022-10-07 20:30:30 +0200

    Update comment for more clarity (#228)
    * fix: update comment
    
    * Update src/nile/cli.py
    
    Co-authored-by: Andrew Fleming <fleming.andrew@protonmail.com>
    
    Co-authored-by: Andrew Fleming <fleming.andrew@protonmail.com>

--- src/nile/cli.py
+++ src/nile/cli.py
@@ -126,7 +126,8 @@ def send(signer, address_or_alias, method, params, network, max_fee=None):
             method, address_or_alias, [x for x in params]
         )
     )
-    # Account.send is part of the public API, so it accepts addresses as string
+    # address_or_alias is not normalized first here because
+    # Account.send is part of Nile's public API and can accept hex addresses
     out = account.send(address_or_alias, method, params, max_fee=max_fee)
     print(out)
 
