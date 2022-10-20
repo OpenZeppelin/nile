195a24bb07df3180de02c5f8d8ee5c49bc3b2f88
Andrew <fleming-andrew@protonmail.com>
2022-10-19 23:44:01 -0400

    add tests for common funcs

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,38 +1,22 @@
 """Tests for common library."""
-from unittest.mock import patch
-
 import pytest
 
-from nile.common import BUILD_DIRECTORY, prepare_params, run_command, stringify
+from nile.common import (
+    capture_stdout,
+    get_feeder_url,
+    get_gateway_url,
+    parse_information,
+    prepare_params,
+    stringify,
+)
 
-CONTRACT = "contract"
-OPERATION = "invoke"
 NETWORK = "goerli"
 ARGS = ["1", "2", "3"]
 LIST1 = [1, 2, 3]
 LIST2 = [1, 2, 3, [4, 5, 6]]
 LIST3 = [1, 2, 3, [4, 5, 6, [7, 8, 9]]]
-
-
-@pytest.mark.parametrize("operation", ["invoke", "call"])
-@patch("nile.common.subprocess.check_output")
-def test_run_command(mock_subprocess, operation):
-
-    run_command(
-        contract_name=CONTRACT, network=NETWORK, operation=operation, arguments=ARGS
-    )
-
-    mock_subprocess.assert_called_once_with(
-        [
-            "starknet",
-            operation,
-            "--contract",
-            f"{BUILD_DIRECTORY}/{CONTRACT}.json",
-            "--inputs",
-            *ARGS,
-            "--no_wallet",
-        ]
-    )
+STDOUT_1 = "SDTOUT_1"
+STDOUT_2 = "SDTOUT_2"
 
 
 @pytest.mark.parametrize(
@@ -62,3 +46,49 @@ def test_stringify(args, expected):
 )
 def test_prepare_params(args, expected):
     assert prepare_params(args) == expected
+
+
+@pytest.mark.parametrize(
+    "network, expected",
+    [
+        ("localhost", "http://127.0.0.1:5050/"),
+        ("goerli", "https://alpha4.starknet.io/gateway"),
+        ("mainnet", "https://alpha-mainnet.starknet.io/gateway"),
+    ],
+)
+def test_get_gateway_url(network, expected):
+    url = get_gateway_url(network)
+    assert url == expected
+
+
+@pytest.mark.parametrize(
+    "network, expected",
+    [
+        ("localhost", "http://127.0.0.1:5050/"),
+        ("goerli", "https://alpha4.starknet.io/feeder_gateway"),
+        ("mainnet", "https://alpha-mainnet.starknet.io/feeder_gateway"),
+    ],
+)
+def test_get_feeder_url(network, expected):
+    url = get_feeder_url(network)
+    assert url == expected
+
+
+@pytest.mark.asyncio
+async def test_capture_stdout():
+    async def helper():
+        print(STDOUT_1)
+        print(STDOUT_2)
+
+    output = await capture_stdout(helper())
+
+    assert f"{STDOUT_1}\n{STDOUT_2}" in output
+
+
+def test_parse_information():
+    a = format(1, "#066x")
+    b = format(2, "#066x")
+    target = f"Formatting 'a': {a}. Formatting 'b': {b}."
+
+    _a, _b = parse_information(target)
+    assert _a, _b == (a, b)
