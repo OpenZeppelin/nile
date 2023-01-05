cc6ba1dc4a4160316de049014e64884c02b8f09b
Eric Nordelo <eric.nordelo39@gmail.com>
2023-01-05 18:38:50 +0100

    feat: roll-back readme

--- src/nile/core/run.py
+++ src/nile/core/run.py
@@ -9,7 +9,7 @@ from nile.nre import NileRuntimeEnvironment
 async def run(path, network):
     """Run nile scripts passing on the NRE object."""
     logger = logging.getLogger()
-    logger.disabled = True
+    logger.setLevel(logging.ERROR)
     script = SourceFileLoader("script", path).load_module()
     nre = NileRuntimeEnvironment(network)
     await script.run(nre)
